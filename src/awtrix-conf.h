e550c04beb52031cdb3d837fe7fa1d3bf45c8767
徐 瑞康 Xu Ruikang <38579360+monsterlady@users.noreply.github.com>
2020-03-08 20:20:40 +0100

    Add files via upload

---
+++ src/awtrix-conf.h
@@ -0,0 +1,14 @@
+///////////////////////// AWTRIX CONFIG /////////////////////////
+
+// Wifi Config
+const char *ssid = "BestitzSub";
+const char *password = "Bladeandsoul1";
+char *awtrix_server = "172.31.39.249";
+
+//#define USB_CONNECTION
+//#define MATRIX_MODEV2
+
+/// LDR Config
+#define LDR_RESISTOR 1000 //ohms
+#define LDR_PIN A0
+#define LDR_PHOTOCELL LightDependentResistor::GL5516
